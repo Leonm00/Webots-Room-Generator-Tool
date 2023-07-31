1d665b23a758d9a9770432e367517b0721f6a6e5
Leonm00 <125961151+Leonm00@users.noreply.github.com>
2023-07-31 15:02:12 +0200

    Update main_Input.py

--- Webots_Tool/main_Input.py
+++ Webots_Tool/main_Input.py
@@ -8,15 +8,15 @@ import Graphic_Interface_Input as GI
 """_Gets all user inputs required for the main file_ 
 """
 
-def get_Choice() :
-    """_Gets choice of Webots Room Generator Tool mode_
+def get_isSet_Choice() :
+    """_Gets choice between generating a set of Webots World or juste a single Webots World_
 
     Returns:
-        c (_int_): _Correspond to the choice mode_
+        c (_int_): _Correspond to the selected choice_
     """
-    print("Please choose the feature you want to use for this Webots Room Generator Tool : ")
-    print(" 1 - Manual ")
-    print(" 2 - With an image (does not work well for complex plans) ")
+    print("Welcome to this Tool. How many Webots World do you want to generate ? ")
+    print(" 1 - A set of Webots World ")
+    print(" 2 - A single Webots World ")
     c = input("Choice (int) : ")
     isOK = False 
     while isOK == False : 
@@ -30,54 +30,7 @@ def get_Choice() :
             print("     Not an int - Please choose between 1 and 2")
             c = input("Choice (int) : ")
     return int(c)
-
-def get_name() :
-    """_Gets the image file name_
-
-    Returns:
-        n (_str_): _Corresponds to the name of the selected image_
-    """
-    n = input ("Give the image name (don't forget the file format) : ")
-    isOK = False 
-    while isOK == False : 
-        if len(n) != 0 :
-            try : 
-                with open("images/" + n): isOK = True
-            except IOError:
-                print("     Image file name does not exist - Please retry")
-                n = input ("Give the image name (don't forget the file format) : ")
-        else : 
-            print("     Empty name - Please retry")
-            n = input ("Give the image name (don't forget the file format) : ")
-    return str(n)
-
-def get_min_max(W_room) : 
-    """_Gets minimum and maximum (x y) coordinates_
-
-    Args:
-        W_room (_list_): _List containing all points corresponding to the walls_
-
-    Returns:
-        min_x (_int_): _The smallest x among all x coordinates in the W_room list_
-        max_x (_int_): _The largest x among all x coordinates in the W_room list_
-        min_y (_int_): _The smallest y among all y coordinates in the W_room list_
-        max_y (_int_): _The largest y among all y coordinates in the W_room list_
-    """
-    min_x = 10000
-    min_y = 10000
-    max_x = 0
-    max_y = 0
-    for e in W_room : 
-        if e[0] < min_x :
-            min_x = e[0]
-        if e[0] > max_x : 
-            max_x = e[0]
-        if e[1] < min_y :
-            min_y = e[1]
-        if e[1] > max_y :
-            max_y = e[1]
-    return min_x, min_y, max_x, max_y
-
+    
 def get_Config_Choice() :
     """_Gets choice between using the Webots Tools and modifying the Default Configuration_
 
@@ -141,6 +94,9 @@ def get_Default_Choice() :
             choice = input("    Press 'Y' for Yes or 'N' for No : ")
     return choice
 
+# Si pas de problème, on affiche le fichier des paramètres par défaut
+# Si problème, on retourne la valeur False et dans get_Config_Choice(), on regarde si l'utilisateur a juste choisi d'utiliser l'outil malgré cet erreur
+# Si oui, on retourne la valeur 4 pour faire savoir que dans les étapes suivantes, on ne proposera pas à l'user d'utiliser ces paramètres par défaut
 def get_default_configuration() :
     """_Displays the current Default Configuration_
 
@@ -382,7 +338,7 @@ def get_size_one_length() :
     """_Gets the length (in meters) of the horizontal line_
 
     Returns:
-        l (_float_): _Corresponds to the length of the selected line_
+        l (_float_): _Corresponds to the length of the horizontal line_
     """
     l = input("Define the length of the selected line (in meters), the width will be adjusted accordingly : ")
     isOK = False
@@ -398,5 +354,75 @@ def get_size_one_length() :
                 isOK = True
         else :
             print("     Not a float - Please retry")
-            l = input("Define the length of the horizontal line (in meters) : ")
+            l = input("Define the length of the selected line (in meters) : ")
     return float(l)
+
+def get_Choice() :
+    """_Gets choice of Webots Room Generator Tool mode_
+
+    Returns:
+        c (_int_): _Correspond to the choice mode_
+    """
+    print("Please choose the feature you want to use for this Webots Room Generator Tool : ")
+    print(" 1 - Manual ")
+    print(" 2 - With an image (does not work well for complex plans) ")
+    c = input("Choice (int) : ")
+    isOK = False 
+    while isOK == False : 
+        if C.check_input(c) == 'int' :
+            if int(c) == 1 or int(c) == 2 :
+                isOK = True
+            else :  
+                print("     Not an expected int - Please choose between 1 and 2")
+                c = input("Choice (int) : ")
+        else :
+            print("     Not an int - Please choose between 1 and 2")
+            c = input("Choice (int) : ")
+    return int(c)
+
+def get_name() :
+    """_Gets the image file name_
+
+    Returns:
+        n (_str_): _Corresponds to the name of the selected image_
+    """
+    n = input ("Give the image name (don't forget the file format) : ")
+    isOK = False 
+    while isOK == False : 
+        if len(n) != 0 :
+            try : 
+                with open("images/" + n): isOK = True
+            except IOError:
+                print("     Image file name does not exist - Please retry")
+                n = input ("Give the image name (don't forget the file format) : ")
+        else : 
+            print("     Empty name - Please retry")
+            n = input ("Give the image name (don't forget the file format) : ")
+    return str(n)
+
+def get_min_max(W_room) : 
+    """_Gets minimum and maximum (x y) coordinates_
+
+    Args:
+        W_room (_list_): _List containing all points corresponding to the walls_
+
+    Returns:
+        min_x (_int_): _The smallest x among all x coordinates in the W_room list_
+        max_x (_int_): _The largest x among all x coordinates in the W_room list_
+        min_y (_int_): _The smallest y among all y coordinates in the W_room list_
+        max_y (_int_): _The largest y among all y coordinates in the W_room list_
+    """
+    min_x = 10000
+    min_y = 10000
+    max_x = 0
+    max_y = 0
+    for e in W_room : 
+        if e[0] < min_x :
+            min_x = e[0]
+        if e[0] > max_x : 
+            max_x = e[0]
+        if e[1] < min_y :
+            min_y = e[1]
+        if e[1] > max_y :
+            max_y = e[1]
+    return int(min_x), int(min_y), int(max_x), int(max_y)
